85d24909d154c5cf67167583e84a79248836f5d8
pikachuim <pikachuim@qq.com>
2019-08-25 20:32:43 +0800

    pika-update

---
+++ backup/pika-signtool.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef 皮卡丘签名工具_PRIVATE_H
+#define 皮卡丘签名工具_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.1.1.24"
+#define VER_MAJOR	1
+#define VER_MINOR	1
+#define VER_RELEASE	1
+#define VER_BUILD	24
+#define COMPANY_NAME	""
+#define FILE_VERSION	"1.1.1.24"
+#define FILE_DESCRIPTION	"Pikachu Code Signature Tool"
+#define INTERNAL_NAME	"Pikachu Code Signature Tool"
+#define LEGAL_COPYRIGHT	"Copyright 2019 Pikachuim"
+#define LEGAL_TRADEMARKS	"Pikachu Code Signature Tool"
+#define ORIGINAL_FILENAME	"Pikasign"
+#define PRODUCT_NAME	"Pikachu Code Signature Tool"
+#define PRODUCT_VERSION	"1.1.1.24"
+
+#endif /*皮卡丘签名工具_PRIVATE_H*/
---
+++ backup/pika-signtool_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef PIKA-SIGNTOOL_PRIVATE_H
+#define PIKA-SIGNTOOL_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.1.1.26"
+#define VER_MAJOR	1
+#define VER_MINOR	1
+#define VER_RELEASE	1
+#define VER_BUILD	26
+#define COMPANY_NAME	""
+#define FILE_VERSION	"1.1.1.26"
+#define FILE_DESCRIPTION	"Pikachu Code Signature Tool"
+#define INTERNAL_NAME	"Pikachu Code Signature Tool"
+#define LEGAL_COPYRIGHT	"Copyright 2019 Pikachuim"
+#define LEGAL_TRADEMARKS	"Pikachu Code Signature Tool"
+#define ORIGINAL_FILENAME	"Pikasign"
+#define PRODUCT_NAME	"Pikachu Code Signature Tool"
+#define PRODUCT_VERSION	"1.1.1.26"
+
+#endif /*PIKA-SIGNTOOL_PRIVATE_H*/
